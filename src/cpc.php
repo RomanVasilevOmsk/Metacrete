3bd02a5fe1d37cf49f2e30188870f9942e92d7ac
Roman <zvezdarusy@gmail.com>
2017-09-27 12:08:15 +0600

    Change title time on the form and modal window

--- src/cpc.php
+++ src/cpc.php
@@ -31,6 +31,24 @@
 			return 'Metacrete - сухие ремонтные и подливочные смеси';
 	}
 
-	
+    function timeUtm()
+    {
+        $now = new DateTime("now", new DateTimeZone('Europe/Moscow'));
+
+        $morning = clone $now;
+        $morning->setTime(6, 30);
+
+        $evening = clone $now;
+        $evening->setTime(15, 0);
+
+        $isWeekend = $now->format('N') >= 6;
+        if ($now >= $morning && $now < $evening && !$isWeekend) {
+            return 'Перезвоним в течение 4 минут';
+        } else {
+            return 'Мы перезвоним Вам в рабочее время.';
+        }
+
+
+    }
 	
 ?>
